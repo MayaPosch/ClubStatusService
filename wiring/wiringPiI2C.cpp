6be37d6cf23cabc0f6cd930cb5e1d9ca8b902f87
Maya Posch <maya@nyanko.ws>
2018-09-20 22:38:21 +0200

    Implemented basic I2C device mock.

--- wiring/wiringPiI2C.cpp
+++ wiring/wiringPiI2C.cpp
@@ -15,10 +15,19 @@
 
 #include "wiringPiI2C.h"
 
+#include "../club.h"
+
+#include <Poco/NumberFormatter.h>
+
+using namespace Poco;
+
 
 int wiringPiI2CSetup(const int devId) {
 	// Device ID is ignored as the test hard-codes the device.
+	Club::log(LOG_INFO, "wiringPiI2CSetup: setting up device ID: 0x" 
+							+ NumberFormatter::formatHex(devId));
 	
+	// Return the device handle.
 	return 0;
 }
 
@@ -26,7 +35,10 @@ int wiringPiI2CSetup(const int devId) {
 int wiringPiI2CWriteReg8(int fd, int reg, int data) {
 	// Write the provided data to the specified register of the simulated device.
 	
-	// TODO:
+	// Here we could simulate one or more devices, or just output what we received here.
+	Club::log(LOG_INFO, "wiringPiI2CWriteReg8: Device handle 0x" + NumberFormatter::formatHex(fd) 
+							+ ", Register 0x" + NumberFormatter::formatHex(reg)
+							+ " set to: 0x" + NumberFormatter::formatHex(data));
 	
 	return 0;
 }
